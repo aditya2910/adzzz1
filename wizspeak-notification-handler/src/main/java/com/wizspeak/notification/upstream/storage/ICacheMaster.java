3fd2957bac87de0b4cdafe1794b598ea0968edec
aditya2910 <aditya.kumar@ge.com>
2017-10-15 12:30:12 +0530

    added add user api

--- wizspeak-notification-handler/src/main/java/com/wizspeak/notification/upstream/storage/ICacheMaster.java
+++ wizspeak-notification-handler/src/main/java/com/wizspeak/notification/upstream/storage/ICacheMaster.java
@@ -4,10 +4,22 @@ import java.util.List;
 
 /**
  * this class would be point for storing and retrieving any data from underlying cache or database impl 
- * @author 212410796
  *
  */
 public interface ICacheMaster {
-	public boolean addElements(String key, List<String> subsribers);
-	public List<String> getAll(String key);
+	/**
+	 * get list of interested parties from cache
+	 * @param postId
+	 * @return
+	 */
+	public List<String> getListOfInterestedPartiesFromCache(String postId);
+	
+	/**
+	 * add new actor to the list of interested parties in cache after notification creation.
+	 * 
+	 * @param key
+	 * @param subsribers
+	 */
+	public void addElements( String postId, String actorId);
+	
 }
