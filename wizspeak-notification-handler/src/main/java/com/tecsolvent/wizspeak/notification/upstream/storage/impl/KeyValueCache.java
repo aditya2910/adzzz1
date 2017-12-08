5d8cfc5dd7ebb576fd1ff4485c5ec4d888d852db
adzzz <mailfor.aditya2910@gmail.com>
2017-12-08 23:24:25 +0530

    added methods in redis to store java object and map

--- wizspeak-notification-handler/src/main/java/com/tecsolvent/wizspeak/notification/upstream/storage/impl/KeyValueCache.java
+++ wizspeak-notification-handler/src/main/java/com/tecsolvent/wizspeak/notification/upstream/storage/impl/KeyValueCache.java
@@ -1,11 +1,15 @@
 package com.tecsolvent.wizspeak.notification.upstream.storage.impl;
 
 import java.util.List;
+import java.util.Map;
 
 import org.redisson.Redisson;
+import org.redisson.api.RBucket;
+import org.redisson.api.RMap;
 import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
 
+import com.tecsolvent.wizspeak.notification.dao.Notification;
 import com.tecsolvent.wizspeak.notification.exception.InterestedPartiesHandlerException;
 import com.tecsolvent.wizspeak.notification.upstream.storage.api.IKeyValueCache;
 
@@ -36,7 +40,7 @@ public class KeyValueCache implements IKeyValueCache {
 	public void initIt() throws Exception {
 		Config config = new Config();
 		System.out.println("redis: " + redisHost + ":" + redisPort );
-    	config.useSingleServer().setAddress( redisHost + ":" + redisPort );
+     	config.useSingleServer().setAddress( redisHost + ":" + redisPort );
         redisson = Redisson.create(config);
 	}
 
@@ -47,6 +51,27 @@ public class KeyValueCache implements IKeyValueCache {
 
 	public List<String> getAll(String key) throws InterestedPartiesHandlerException {
 		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	// TODO: add below methods to IKeyValueCache when they are discussed to be fine
+	public void addNotificationObjectsToStore(String key, Notification notification) throws InterestedPartiesHandlerException {
+		RBucket<Notification> bucket = redisson.getBucket(key);
+		bucket.set(notification);
+	}
+	
+	public Notification getNotificationObjectsFromStore(String key) throws InterestedPartiesHandlerException {
+		RBucket<Notification> bucket = redisson.getBucket(key);
+		return bucket.get();
+	}
+	
+	public void addNotificationsMapToStore(String key, Map<String,Notification> mapOfNotifications) throws InterestedPartiesHandlerException {
+		RMap<String, Notification> rmap = redisson.getMap(key);
+		rmap.putAll(mapOfNotifications);
+	}
+	
+	public Map<String,Notification> getNotificationsMapFromStore(String key, Map<String,Notification> mapOfNotifications) throws InterestedPartiesHandlerException {
+		//https://github.com/redisson/redisson/wiki/7.-Distributed-collections
 		return null;
 	}
 
