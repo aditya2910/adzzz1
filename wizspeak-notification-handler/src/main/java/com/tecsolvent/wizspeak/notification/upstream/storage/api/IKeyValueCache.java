c20dc7a5e09e6ee57b79929ea0ebc3a4995bd231
adzzz <mailfor.aditya2910@gmail.com>
2017-12-09 08:26:06 +0530

    todo

--- wizspeak-notification-handler/src/main/java/com/tecsolvent/wizspeak/notification/upstream/storage/api/IKeyValueCache.java
+++ wizspeak-notification-handler/src/main/java/com/tecsolvent/wizspeak/notification/upstream/storage/api/IKeyValueCache.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import com.tecsolvent.wizspeak.notification.exception.InterestedPartiesHandlerException;
 
+//TODO: see if having DataAccessService is also needed with KeyValueCache
 public interface IKeyValueCache {
 	
 	public boolean addElements(String key, List<String> subsribers) throws InterestedPartiesHandlerException;
