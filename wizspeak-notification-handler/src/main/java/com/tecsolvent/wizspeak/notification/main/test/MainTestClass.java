c6e62a3d398e9cb46b67f7fc071f46b65f524ac0
Aditya Kumar <adityakumar@fico.com>
2017-12-18 20:35:20 +0530

    test using javaspark

--- wizspeak-notification-handler/src/main/java/com/tecsolvent/wizspeak/notification/main/test/MainTestClass.java
+++ wizspeak-notification-handler/src/main/java/com/tecsolvent/wizspeak/notification/main/test/MainTestClass.java
@@ -2,14 +2,35 @@ package com.tecsolvent.wizspeak.notification.main.test;
 
 import static spark.Spark.*;
 
+import java.util.List;
+
+import com.tecsolvent.wizspeak.notification.dao.Notification;
+import com.tecsolvent.wizspeak.notification.services.NotificationLogicException;
+import com.tecsolvent.wizspeak.notification.services.ViewNotificationService;
+import com.tecsolvent.wizspeak.notification.services.ViewNotificationServiceImpl;
+
+import spark.Request;
+import spark.Response;
+import spark.Route;
+
 public class MainTestClass {
 	
 	public static void main(String[] args) {
-		System.out.println("Waiting for requests.....");
-        get("/hello", (req, res) -> createResponse());
-    }
-
-	private static String createResponse() {
+	get("/hello", new Route() {
+		@Override
+		public Object handle(Request request, Response response) throws NotificationLogicException {
+		// process request
+			ViewNotificationService vns = new ViewNotificationServiceImpl();
+			return vns.get(1234);
+		}
+		});
+	}
+	private static String createResponse() throws NotificationLogicException {
+		//1234
+		ViewNotificationService vns = new ViewNotificationServiceImpl();
+			List<Notification> listOfNotification = vns.get(1234);
+			System.out.println("listOfNotification size: " + listOfNotification.size());
+		
 		return "Hello World";
 	}
 
