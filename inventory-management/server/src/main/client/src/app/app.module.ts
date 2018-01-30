c0e9428baf1fdeef1b11c0a6b4c7eb7fcfbf6527
Aditya Kumar <adityakumar@fico.com>
2018-01-30 11:11:14 +0530

    router link working

--- inventory-management/server/src/main/client/src/app/app.module.ts
+++ inventory-management/server/src/main/client/src/app/app.module.ts
@@ -1,13 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule } from "@angular/router";
 
 import { AppComponent } from './app.component';
 import {HttpModule} from "@angular/http";
 import { RawmaterialListComponent } from './rawmaterial-list/rawmaterial-list.component';
 import {RawmaterialService} from "./shared/rawmaterial/rawmaterial.service";
 import { RawmaretialAddComponent } from './rawmaretial-add/rawmaretial-add.component';
-
 
 @NgModule({
   declarations: [
@@ -17,7 +16,14 @@ import { RawmaretialAddComponent } from './rawmaretial-add/rawmaretial-add.compo
   ],
   imports: [
     BrowserModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot([
+      {
+        path: 'add',
+        component: RawmaretialAddComponent
+      },
+      { path : "", redirectTo: "/", pathMatch: "full" }
+    ])
   ],
   providers: [RawmaterialService],
   bootstrap: [AppComponent]
