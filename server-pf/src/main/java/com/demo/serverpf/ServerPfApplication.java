9d4a896d3818facb38a0883c48185bfbc598af4e
Aditya Kumar <adityakumar@fico.com>
2018-02-09 21:26:12 +0530

    pf project start

---
+++ server-pf/src/main/java/com/demo/serverpf/ServerPfApplication.java
@@ -0,0 +1,12 @@
+package com.demo.serverpf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerPfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerPfApplication.class, args);
+	}
+}
