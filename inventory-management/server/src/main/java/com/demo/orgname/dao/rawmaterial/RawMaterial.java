eede358b8d68f11b6721e2a5ef4f44f11105beb9
Aditya Kumar <adityakumar@fico.com>
2018-02-10 08:56:40 +0530

    raw material

--- inventory-management/server/src/main/java/com/demo/orgname/dao/rawmaterial/RawMaterial.java
+++ inventory-management/server/src/main/java/com/demo/orgname/dao/rawmaterial/RawMaterial.java
@@ -26,17 +26,38 @@ public class RawMaterial {
     
     public RawMaterial() {
 	}
-    
+
 	public String getId() {
 		return id;
 	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
 	public String getName() {
 		return name;
 	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
 	public String getUnit() {
 		return unit;
 	}
+
+	public void setUnit(String unit) {
+		this.unit = unit;
+	}
+
 	public String getType() {
 		return type;
 	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+    
+	
 }
