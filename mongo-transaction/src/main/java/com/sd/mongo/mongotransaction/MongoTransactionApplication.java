616d5f6a763b79a7d0121ab85bba8137bc3258fa
Aditya Kumar <mailfor.aditya2910@gmail.com>
2018-04-13 16:24:34 +0530

    initial-project-check-in

---
+++ mongo-transaction/src/main/java/com/sd/mongo/mongotransaction/MongoTransactionApplication.java
@@ -0,0 +1,12 @@
+package com.sd.mongo.mongotransaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongoTransactionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongoTransactionApplication.class, args);
+	}
+}
