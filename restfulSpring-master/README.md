ca7e962b3b6c55d156e3c2bf20db3ac505131107
Aditya Kumar <adityakumar@fico.com>
2019-03-06 11:06:12 +0530

    new app - REST service app without spring boot dependencies

---
+++ restfulSpring-master/README.md
@@ -0,0 +1,6 @@
+# Restful Spring
+
+- Build with: mvn clean install
+- Run with: mvn jetty:start
+
+A minimal starter project for a Restful Spring application.
---
+++ restfulSpring-master/src/main/java/biz/tugay/restfulspring/config/RestfulHello.java
@@ -0,0 +1,65 @@
+package biz.tugay.restfulspring.config;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController(value = "/")
+public class RestfulHello {
+	
+
+    @RequestMapping(value = "hello")
+    public ResponseEntity<String> sayHello() throws MalformedURLException  {
+        final HttpHeaders httpHeaders= new HttpHeaders();
+        httpHeaders.setContentType(MediaType.APPLICATION_JSON);
+        
+
+        
+//        try {
+//
+//    		URL url = new URL("http://localhost:6082/bootelkzipkin2");
+//    		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+//    		conn.setRequestMethod("GET");
+//    		conn.setRequestProperty("Accept", "application/json");
+//
+//    		if (conn.getResponseCode() != 200) {
+//    			throw new RuntimeException("Failed : HTTP error code : "
+//    					+ conn.getResponseCode());
+//    		}
+//
+//    		BufferedReader br = new BufferedReader(new InputStreamReader(
+//    			(conn.getInputStream())));
+//
+//    		String output;
+//    		System.out.println("Output from Server .... \n");
+//    		while ((output = br.readLine()) != null) {
+//    			System.out.println(output);
+//    		}
+//
+//    		conn.disconnect();
+//
+//    	  } catch (MalformedURLException e) {
+//
+//    		e.printStackTrace();
+//
+//    	  } catch (IOException e) {
+//
+//    		e.printStackTrace();
+//
+//    	  }
+        
+        return new ResponseEntity<String>("{\"msg\": \"Hello World\"}", httpHeaders, HttpStatus.OK);
+    }
+    
+}
