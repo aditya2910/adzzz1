8145ce5b758674bd795e73bdcaad5a835b831181
adityakumar <adityakumar@fico.com>
2019-06-20 11:20:50 +0530

    new spring data code for updated mysql authentication mechanism

--- spring-data-proj/src/main/java/com/demo/orgname/dao/rawmaterial/RawMaterialRepository.java
+++
@@ -1,7 +0,0 @@
-package com.demo.orgname.dao.rawmaterial;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface RawMaterialRepository extends CrudRepository<RawMaterial, String> {
-
-}
