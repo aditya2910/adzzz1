4ae4741c935890a7f36e77b8ecca209753f86bba
adityakumar <adityakumar@fico.com>
2019-07-17 15:38:14 +0530

    singly linked list CRU operations

--- java-dsa-samples/src/main/java/com/adzzz/dsa/singlylinkedlist/crud/SinglyLinkedListCrud.java
+++ java-dsa-samples/src/main/java/com/adzzz/dsa/singlylinkedlist/crud/SinglyLinkedListCrud.java
@@ -2,4 +2,51 @@ package com.adzzz.dsa.singlylinkedlist.crud;
 
 public class SinglyLinkedListCrud {
 
+	public static void main(String[] args) {
+		// Create singly list with 4 items
+		SinglyNode list = createSinglyLinkedList();
+		// Read singly list with 4 items
+		printListContent(list);
+		
+		// Update node with 3 to 9
+		SinglyNode updatedList = updateSinglyLinkedList(list, 3, 9);
+		printListContent(updatedList);
+	}
+
+	private static SinglyNode updateSinglyLinkedList(SinglyNode list, int oldValue, int newValue) {
+		SinglyNode matchedNode = null;
+		for (SinglyNode tmp = list.getNode(); tmp != null; tmp = tmp.getNode()) {
+			if (oldValue == tmp.getValue()) {
+				matchedNode = tmp;
+			}
+		}
+		
+	    matchedNode.setValue(newValue); 
+	    
+	    return list;
+	}
+	
+
+
+	private static SinglyNode createSinglyLinkedList() {
+		SinglyNode node1 = new SinglyNode(1);
+		SinglyNode node2 = new SinglyNode(2);
+		node1.setNode(node2);
+		SinglyNode node3 = new SinglyNode(3);
+		node2.setNode(node3);
+		SinglyNode node4 = new SinglyNode(4);
+		node3.setNode(node4);
+		
+		
+		
+		return node1;
+	}
+
+	private static void printListContent(SinglyNode list) {
+		System.out.println(list.getValue());
+		if(list.getNode() == null) {
+			return;
+		}
+		printListContent(list.getNode());
+	}
 }
---
+++ java-dsa-samples/src/main/java/com/adzzz/dsa/singlylinkedlist/crud/SinglyNode.java
@@ -0,0 +1,41 @@
+package com.adzzz.dsa.singlylinkedlist.crud;
+
+
+public class SinglyNode implements Cloneable {
+
+	private int value;
+	private SinglyNode node;
+	
+	public SinglyNode(int value) {
+		super();
+		this.value = value;
+		this.node = null;
+	}
+
+	public int getValue() {
+		return value;
+	}
+
+	public void setValue(int value) {
+		this.value = value;
+	}
+
+	public SinglyNode getNode() {
+		return node;
+	}
+
+	public void setNode(SinglyNode node) {
+		this.node = node;
+	}
+
+	@Override
+	public String toString() {
+		return "SinglyNode [value=" + value + ", node=" + node + "]";
+	}
+	
+	@Override
+	public Object clone()throws CloneNotSupportedException{  
+		return super.clone();  
+		}  
+	
+}
